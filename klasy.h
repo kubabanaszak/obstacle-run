cce876ec35587b86a5140e688fbb356281ecb582
OliwiaBialek <oliwia.o.bialek@gmail.com>
2025-05-16 21:08:02 +0200

    utworzenie klasy.h
    dodanie szkieletów klas

---
+++ klasy.h
@@ -0,0 +1,90 @@
+#ifndef KLASY_H
+#define KLASY_H
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+
+class Game
+{
+public:
+
+private:
+
+};
+
+class Player
+{
+private:
+
+public:
+
+};
+
+class Text
+{
+protected:
+
+};
+
+class Menu
+{
+private:
+
+public:
+
+};
+
+class Score : public Text
+{
+private:
+
+public:
+   
+};
+
+class Obstacle
+{
+protected:
+    
+public:
+
+};
+
+class Up : public Obstacle
+{
+public:
+
+};
+
+class Down : public Obstacle
+{
+public:
+
+};
+
+class Both : public Obstacle
+{
+public:
+
+};
+
+class HighScore
+{
+public:
+
+private:
+
+};
+
+class PowerUp
+{
+public:
+
+};
+
+#endif // KLASY_H
