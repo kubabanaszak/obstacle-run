3833f61d52d7827093b2e4c9938d8798baa465cb
kubabanaszak <jakub.banaszak997@wp.pl>
2025-05-18 20:59:50 +0200

    edycja klasy Player + dodanie struktur w klasy.cpp

--- klasy.cpp
+++ klasy.cpp
@@ -11,8 +11,47 @@
 Game::Game() {};
 Game::~Game() {};
 
-Player::Player() {};
+Player::Player() 
+{
+    shape.setSize(sf::Vector2f(100, 100));
+    t[0].loadFromFile("bieg_1.png");
+    t[1].loadFromFile("bieg_2.png");
+    t[2].loadFromFile("skok.png");
+    t[3].loadFromFile("wslizg.png");
+    t[4].loadFromFile("emoji.png");
+    shape.setTexture(&t[0]);
+};
+
 Player::~Player() {};
+void Player::setPosition(float x, float y) {shape.setPosition(x, y);}
+
+sf::Vector2f Player::getPosition() {return shape.getPosition();}
+
+sf::RectangleShape& Player::getShape() {return shape;}
+
+void Player::textureChange()
+{
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        shape.setTexture(&t[2]);
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        shape.setTexture(&t[3]);
+    }
+    if (textureClock.getElapsedTime().asSeconds() >= switchTime) {
+        if (useFirstTexture) {
+            shape.setTexture(&t[1]);
+        }
+         else
+        {
+            shape.setTexture(&t[0]);
+        }
+        useFirstTexture = !useFirstTexture;
+        textureClock.restart();
+    }
+}
+void Player::endTexture() {shape.setTexture(&t[4]);}
 
 Text::Text() {};
 Text::~Text() {};
