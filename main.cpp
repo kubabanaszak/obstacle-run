f0edc9df1a0a86104b63cd950d13a6ed66e7938e
kubabanaszak <jakub.banaszak997@wp.pl>
2025-05-18 20:49:38 +0200

    dodanie szkieletow funkcji oraz render okna w main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,61 @@
 #include <sstream>
 #include <fstream>
 
+void idPlik();
+void GetData();
+void ZapiszDane();
+void PobierzWyniki();
+void ZapiszWyniki();
+void Sortuj();
+void movement();
+void los();
+
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(600, 900), "Obstacle Run", sf::Style::Close);
+    bool gameOver = false;
+    bool moveLeft = false;
+    bool moveRight = false;
+    bool gameStarted = false;
     return 0;
+}
+
+void movement()
+{
+    
+}
+
+void los()
+{
+    
+}
+
+void idPlik()
+{
+
+}
+
+void GetData()
+{
+
+}
+
+void ZapiszDane()
+{
+
+}
+
+void PobierzWyniki()
+{
+
+}
+
+void Sortuj()
+{
+
+}
+
+void ZapiszWyniki()
+{
+
 }
